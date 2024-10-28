da7e5f423bb19b98fda4343e8dd22eb80c509eb3
YeonJi <122419518+jiyeon0926@users.noreply.github.com>
2024-10-28 16:35:45 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+            }
+        }
+        
+        for (int num : list) {
+            answer += num;
+        }
+        
+        return answer;
+    }
+}
