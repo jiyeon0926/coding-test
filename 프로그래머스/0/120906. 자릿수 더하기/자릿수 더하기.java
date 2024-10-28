d714f88b464b835d83c9e391b223da83b28a54ae
YeonJi <122419518+jiyeon0926@users.noreply.github.com>
2024-10-28 10:06:49 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        List<Integer> list = new ArrayList<>();
+
+        while (n > 0) {
+            list.add(n % 10);
+            n /= 10;
+        }
+        
+        for (int num : list) {
+            answer += num;
+        }
+        
+        return answer;
+    }
+}
